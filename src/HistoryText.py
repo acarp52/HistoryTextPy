c6e5ef41f7abb941ee0a0291442b7049da1e1a33
acarp52 <axc1823@g.rit.edu>
2015-05-13 19:51:22 -0400

    Rearranged functions and added comments

--- src/HistoryText.py
+++ src/HistoryText.py
@@ -5,9 +5,45 @@ from nltk import *
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 
+"""
+Takes a list of tokenized input strings and creates unigrams, bigrams, trigrams,
+and tetragrams. It then prints the results.
+"""
+def make_tokens(toks):
+        myuni = toks
+        mybi = bigrams(toks)
+        mytri = trigrams(toks)
+        mytetra = ngrams(toks,4)
+
+        unifd = nltk.FreqDist(myuni)
+        print(unifd.most_common(5))
+        print('\n')
+
+        bifd = nltk.FreqDist(mybi)
+        print(bifd.most_common(5))
+        print('\n')
+
+        trifd = nltk.FreqDist(mytri)
+        print(trifd.most_common(5))
+        print('\n')
+
+        tetrafd = nltk.FreqDist(mytetra)
+        print(tetrafd.most_common(5))
+
+"""
+Processes a string made from a raw input file and removes punctuation
+and stop words. Returns a string with the cleaned data
+"""
+def process(rawFile):
+    #sentence = sentence.lower()
+    tokenizer = RegexpTokenizer(r'\w+') #need to account for apostrophe
+    tokens = tokenizer.tokenize(rawFile)
+    filtered_words = [w for w in tokens if not w in stopwords.words('english')]
+    #filtered_words = filter(lambda token: token not in stopwords.words('english'), tokens)
+    #print(filtered_words)
+    return " ".join(filtered_words)
+
 def main():
-
-
     file = input('Please enter a file to analyze: ')
     words = ""
 
@@ -24,18 +60,6 @@ def main():
                 break
             words += line   #appends to a string for the data in the file we are interested in
 
-    def process(rawFile):
-        #sentence = sentence.lower()
-        tokenizer = RegexpTokenizer(r'\w+') #need to account for apostrophe
-        tokens = tokenizer.tokenize(rawFile)
-        filtered_words = [w for w in tokens if not w in stopwords.words('english')]
-        #filtered_words = filter(lambda token: token not in stopwords.words('english'), tokens)
-        print(filtered_words)
-        return " ".join(filtered_words)
-
-
-    toks = word_tokenize(process(words))
-
     """
     rTokenizer = RegexpTokenizer(r'\w+')
     rTokenizer.tokenize('Eighty-seven miles to go, yet.  Onward!')
@@ -47,25 +71,8 @@ def main():
     #mytetra = ngrams(toks,4)
 
 
-    myuni = toks
-    mybi = bigrams(toks)
-    mytri = trigrams(toks)
-    mytetra = ngrams(toks,4)
 
-    unifd = nltk.FreqDist(myuni)
-    print(unifd.most_common(5))
-    print('\n')
-
-    bifd = nltk.FreqDist(mybi)
-    print(bifd.most_common(5))
-    print('\n')
-
-    trifd = nltk.FreqDist(mytri)
-    print(trifd.most_common(5))
-    print('\n')
-
-    tetrafd = nltk.FreqDist(mytetra)
-    print(tetrafd.most_common(5))
-
+    tok = word_tokenize(process(words))
+    make_tokens(tok)
 
 main()
