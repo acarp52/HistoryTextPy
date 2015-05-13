6fbf360153ef921a3eeead77f6d46c746ff2890e
acarp52 <axc1823@g.rit.edu>
2015-05-13 19:30:55 -0400

    Strips punctuation (mostly) and metadata and successfully tokenizes input

--- src/HistoryText.py
+++ src/HistoryText.py
@@ -1,14 +1,51 @@
 import nltk
+import string
+import re
 from nltk import *
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
 def main():
 
-    #"test"
-    file = input('Please enter a file to analyze: ')
-    f = open(file)
-    words = f.read()
 
-    toks = word_tokenize(words)
+    file = input('Please enter a file to analyze: ')
+    words = ""
+
+    with open(file) as input_data:
+        # Skips text before the beginning of the interesting block:
+        for line in input_data:
+            match = re.search(r"^[***](.*)[***]$", line)
+            if match:
+                break
+        # Reads text until the end of the block:
+        for line in input_data:  # This keeps reading the file
+            match = re.search(r"^[***](.*)[***]$", line)
+            if match:
+                break
+            words += line   #appends to a string for the data in the file we are interested in
+
+    def process(rawFile):
+        #sentence = sentence.lower()
+        tokenizer = RegexpTokenizer(r'\w+') #need to account for apostrophe
+        tokens = tokenizer.tokenize(rawFile)
+        filtered_words = [w for w in tokens if not w in stopwords.words('english')]
+        #filtered_words = filter(lambda token: token not in stopwords.words('english'), tokens)
+        print(filtered_words)
+        return " ".join(filtered_words)
+
+
+    toks = word_tokenize(process(words))
+
+    """
+    rTokenizer = RegexpTokenizer(r'\w+')
+    rTokenizer.tokenize('Eighty-seven miles to go, yet.  Onward!')
+    """
+
+    #myuni = toks
+    #mybi = bigrams(toks)
+    #mytri = trigrams(toks)
+    #mytetra = ngrams(toks,4)
+
 
     myuni = toks
     mybi = bigrams(toks)
@@ -29,19 +66,6 @@ def main():
 
     tetrafd = nltk.FreqDist(mytetra)
     print(tetrafd.most_common(5))
-
-    """
-    #print(f.read())
-
-    string = "This is a test. A test like this should be disregarded. Carry on."
-    str_bigrams = bigrams(string)
-    #for pair in str_bigrams:
-    #    print(pair)
-
-    bigram_measures = nltk.collocations.BigramAssocMeasures()
-    finder = BigramCollocationFinder.from_words(word_tokenize(f.read()))
-    print(finder.nbest(bigram_measures.pmi, 10))
-    """
 
 
 main()
